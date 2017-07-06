cb25b9cbf732af7bd0cbec9af321c2435cb8aebf
Joshua Bartlett <joshuabartlett@gmail.com>
2017-07-06 10:35:48 -0500

    Better commenting

--- bulma-shortcodes.php
+++ bulma-shortcodes.php
@@ -6,7 +6,6 @@
  * Author: Joshua Bartlett
  * Author URI: https://www.joshuarbartlett.com
  * License: GPL2
- * Donate URI:
  *
  * Bulma Shortcodes is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,65 +22,65 @@ require dirname(__FILE__) . '/inc/functions.php';
 
 class BulmaShortcodesPlugin
 {
-    public function __construct()
-    {
-        if (is_admin()) {
-            add_action('init', array( &$this, 'setup_bulma_shortcodes_plugin'));
-        } else {
-            $this->bulma_register_all_shortcodes();
-        }
-    }
+		public function __construct()
+		{
+				if (is_admin()) {
+						add_action('init', array( &$this, 'setup_bulma_shortcodes_plugin'));
+				} else {
+						$this->bulma_register_all_shortcodes();
+				}
+		}
 
-    public function setup_bulma_shortcodes_plugin()
-    {
-        if (! current_user_can('edit_posts') && ! current_user_can('edit_pages')) {
-            return;
-        }
+		public function setup_bulma_shortcodes_plugin()
+		{
+				if (! current_user_can('edit_posts') && ! current_user_can('edit_pages')) {
+						return;
+				}
 
-        if (get_user_option('rich_editing') !== 'true') {
-            return;
-        }
+				if (get_user_option('rich_editing') !== 'true') {
+						return;
+				}
 
-        add_filter('mce_external_plugins', array( &$this, 'add_bulma_shortcodes_plugin' ));
-        add_filter('mce_buttons', array( &$this, 'add_bulma_shortcodes_toolbar_button' ));
-    }
+				add_filter('mce_external_plugins', array( &$this, 'add_bulma_shortcodes_plugin' ));
+				add_filter('mce_buttons', array( &$this, 'add_bulma_shortcodes_toolbar_button' ));
+		}
 
-    public function add_bulma_shortcodes_plugin()
-    {
-        $plugin_array['bulma_shortcodes_plugin'] = plugin_dir_url(__FILE__) . "bulma_shortcodes_plugin.js";
-        return $plugin_array;
-    }
+		public function add_bulma_shortcodes_plugin()
+		{
+				$plugin_array['bulma_shortcodes_plugin'] = plugin_dir_url(__FILE__) . "bulma_shortcodes_plugin.js";
+				return $plugin_array;
+		}
 
-    public function add_bulma_shortcodes_toolbar_button($buttons)
-    {
-        array_push($buttons, 'bulma_shortcodes');
-        return $buttons;
-    }
+		public function add_bulma_shortcodes_toolbar_button($buttons)
+		{
+				array_push($buttons, 'bulma_shortcodes');
+				return $buttons;
+		}
 
-    private function bulma_register_all_shortcodes()
-    {
-        add_shortcode('bulma-box', 'bulma_sBox');
-        add_shortcode('bulma-notification', 'bulma_sNotification');
-        add_shortcode('bulma-card', 'bulma_sCard');
-        add_shortcode('bulma-card-header', 'bulma_sCardHeader');
-        add_shortcode('bulma-card-content', 'bulma_sCardContent');
-        add_shortcode('bulma-card-footer', 'bulma_sCardFooter');
-        add_shortcode('bulma-icon', 'bulma_sIcon');
-        add_shortcode('bulma-menu', 'bulma_sMenu');
-        add_shortcode('bulma-menu-label', 'bulma_sMenuLabel');
-        add_shortcode('bulma-menu-list', 'bulma_sMenuList');
-        add_shortcode('bulma-button', 'bulma_sButton');
-        add_shortcode('bulma-columns', 'bulma_sColumns');
-        add_shortcode('bulma-columns-1', 'bulma_sColumns');
-        add_shortcode('bulma-columns-2', 'bulma_sColumns');
-        add_shortcode('bulma-columns-3', 'bulma_sColumns');
-        add_shortcode('bulma-columns-4', 'bulma_sColumns');
-        add_shortcode('bulma-column', 'bulma_sColumn');
-        add_shortcode('bulma-column-1', 'bulma_sColumn');
-        add_shortcode('bulma-column-2', 'bulma_sColumn');
-        add_shortcode('bulma-column-3', 'bulma_sColumn');
-        add_shortcode('bulma-column-4', 'bulma_sColumn');
-    }
+		private function bulma_register_all_shortcodes()
+		{
+				add_shortcode('bulma-box', 'bulma_sBox');
+				add_shortcode('bulma-notification', 'bulma_sNotification');
+				add_shortcode('bulma-card', 'bulma_sCard');
+				add_shortcode('bulma-card-header', 'bulma_sCardHeader');
+				add_shortcode('bulma-card-content', 'bulma_sCardContent');
+				add_shortcode('bulma-card-footer', 'bulma_sCardFooter');
+				add_shortcode('bulma-icon', 'bulma_sIcon');
+				add_shortcode('bulma-menu', 'bulma_sMenu');
+				add_shortcode('bulma-menu-label', 'bulma_sMenuLabel');
+				add_shortcode('bulma-menu-list', 'bulma_sMenuList');
+				add_shortcode('bulma-button', 'bulma_sButton');
+				add_shortcode('bulma-columns', 'bulma_sColumns');
+				add_shortcode('bulma-columns-1', 'bulma_sColumns');
+				add_shortcode('bulma-columns-2', 'bulma_sColumns');
+				add_shortcode('bulma-columns-3', 'bulma_sColumns');
+				add_shortcode('bulma-columns-4', 'bulma_sColumns');
+				add_shortcode('bulma-column', 'bulma_sColumn');
+				add_shortcode('bulma-column-1', 'bulma_sColumn');
+				add_shortcode('bulma-column-2', 'bulma_sColumn');
+				add_shortcode('bulma-column-3', 'bulma_sColumn');
+				add_shortcode('bulma-column-4', 'bulma_sColumn');
+		}
 }
 
 $bulma_shortcodes_plugin = new BulmaShortcodesPlugin;
--- inc/functions.php
+++ inc/functions.php
@@ -5,39 +5,44 @@ require dirname(__FILE__) . '/elements.php';
 require dirname(__FILE__) . '/components.php';
 
 /**
-* Registers an editor stylesheet for the theme.
+* Adds the font-awesome library too the editor for the shortcode menu.
 */
-
 add_action('admin_head-post.php', function () {
-    ?>
-    <style>
-    @import url('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    .mce-ico.mce-i-fa {
-        display: inline-block;
-        font: normal normal normal 14px/1 FontAwesome;
-        font-size: inherit;
-        text-rendering: auto;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
-    </style>
-    <?php
+		?>
+		<style>
+		@import url('//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+		.mce-ico.mce-i-fa {
+				display: inline-block;
+				font: normal normal normal 14px/1 FontAwesome;
+				font-size: inherit;
+				text-rendering: auto;
+				-webkit-font-smoothing: antialiased;
+				-moz-osx-font-smoothing: grayscale;
+		}
+		</style>
+		<?php
 
 });
 
+/**
+* Trims added <p> tags from shortcodes for more consistent results.
+*/
 add_filter('the_content', 'bulma_shortcode_empty_paragraph_fix');
 
 function bulma_shortcode_empty_paragraph_fix($content)
 {
-    $array = array(
-        '<p>['    => '[',
-        ']</p>'   => ']',
-        ']<br />' => ']'
-    );
-    return strtr($content, $array);
+		$array = array(
+				'<p>['    => '[',
+				']</p>'   => ']',
+				']<br />' => ']'
+		);
+		return strtr($content, $array);
 }
 
+/**
+* Returns string as an array, split at '|' characters
+*/
 function bulma_split_at_bar($string)
 {
-    return explode('|', $string);
+		return explode('|', $string);
 }
